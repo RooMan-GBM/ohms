8a89edca0f77e93e0220b4a3c8316647cfbc0270
RooMan-GBM <xxroomanxx@gmail.com>
2016-02-05 09:55:56 +0000

    Update main.c
    just the source code, it should work on most platforms without any modification.

--- main.c
+++ main.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-float I=0.00f;
-float V=0.00f;
-float R=0.00f;
-int ver=0;
-int verb=1;
+
+float I=0.00f; //holds the current value
+float V=0.00f; //holds the voltage value
+float R=0.00f; //holds the resistance value
+int ver=0; //holds verion X.0
+int verb=1; // holds 0.X
 
 
 float Ires(float Vin,float Rin) //calculate current v/r
 {
     float Ibk=0.00f;
 
-    if (Vin>0 && Rin>0)
+    if (Vin>0 && Rin>0) // can't devide by zero
     {
         Ibk=(Vin/Rin);
         printf("%0.2fA %0.0fmA\n",Ibk,Ibk*1000);
     }
 
-    else {printf("Error: non-zero values only\n");}
+    else {printf("Error: non-zero values only\n");} // no blanks or chars
 return 0;
 }
 
@@ -26,49 +27,50 @@ float Rres(float Vin,float Iin)//calulate Resistance v/i
 {
     float Rbk=0.00f;
 
-    if (Vin>0 && Iin>0)
+    if (Vin>0 && Iin>0) // can't devide by zero
     {
         Rbk=(Vin/Iin);
         printf("%0.0fΩ %0.3fK\n",Rbk,Rbk/1000);
     }
 
-    else {printf("Error: non-zero values only\n");}
-return 0;
+    else {printf("Error: non-zero values only\n");} // no blanks or chars
+return 0;  // this can be used to return the result back
 
 }
 
-float Vres(float Rin,float Iin)//calulate Resistance v/i
+float Vres(float Rin,float Iin)//calulate voltage r*i
 {
     float Vbk=0.00f;
 
-    if (Rin>0 && Iin>0)
+    if (Rin>0 && Iin>0) // no blanks or zeros, just in case
     {
         Vbk=(Rin*Iin);
         printf("%0.2fV\n",Vbk);
     }
 
     else {printf("Error: non-zero values only\n");}
-return 0;
+return 0; // this can be used to return the result back
 
 }
 
-int main()
+int main() // entry point
 {
-int quit=0;
+int quit=0;//dont quit yet, we've only just started
 
-while(!quit)
+while(!quit) // run forever untill we quit
 
 {
-    printf("\nOHM's V:%d.%d\n",ver,verb);
-    printf("Please select a mode:\n1: Current\n2: Resistance\n3: Voltage\n4: Quit\n********\n\n");
-    int sel=0;
-    scanf("%d",&sel);
-    //quit=sel;
-    if(sel)
+    printf("\nOHM's V:%d.%d\n",ver,verb); // version info
+    printf("Please select a mode:\n1: Current\n2: Resistance\n3: Voltage\n4: Quit\n********\n\n"); // main menu
+    
+    int sel=0;// lets have somewhere to store the input
+    scanf("%d",&sel); // get input
+    
+    if(sel) // is the input a number (i admit this is a crude test)
     {
     switch(sel)
     {
-        case 1://I
+        case 1://Current (I)
         printf("Please enter the Voltage. example; 3.3(V)\n");
         scanf("%f",&V);
 
@@ -78,7 +80,7 @@ while(!quit)
         Ires(V,R);
         break;
 
-        case 2://R
+        case 2://Resistance (R)
         printf("Please enter the Voltage. example; 3.3(V)\n");
         scanf("%f",&V);
 
@@ -88,7 +90,7 @@ while(!quit)
         Rres(V,I);
         break;
 
-        case 3://V
+        case 3://Voltage (V)
         printf("Please enter the Resistance. example; 1000(Ω)\n");
         scanf("%f",&R);
 
@@ -98,13 +100,13 @@ while(!quit)
         Vres(R,I);
         break;
 
-        case 4:
+        case 4: // lets get outta here
         quit=1;
         break;
 
 
     }
     }
-    else {quit=1;}
+    else {quit=1;} // it seems the input is invalid, exit or error message
 }
 }
